3659ff0bcc10fd86b62e64917794178c815416db
VERCHERE Rémi <remi.verchere2@gmail.com>
2025-02-04 22:42:06 +0100

    input rendering OK

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -4,8 +4,8 @@ import { IAOutputAPIInterface } from '@/services/ollama/types'
 import { useState, useEffect, FormEvent } from 'react'
 
 export default function Chat() {
-    const [data, setData] = useState<IAOutputAPIInterface | null>(null)
-    const [isLoading, setLoading] = useState(true)
+    const [answer, setAnswer] = useState<string | null>(null)
+    const [isLoading, setLoading] = useState(false)
 
 
     async function onSubmit(event: FormEvent<HTMLFormElement>) {
@@ -20,24 +20,27 @@ export default function Chat() {
         },
         body: JSON.stringify({prompt: formData.get('prompt')})
       })
-      const iaAnswer = await res.json()
-      setData(iaAnswer as IAOutputAPIInterface)
+      const iaAnswer = await res.json() as IAOutputAPIInterface
+      setAnswer(iaAnswer.response)
       setLoading(false)
 
     }
    
     return (
-      <div className='flex flex-col items-center mt-10'>
-        <form className='grid grid-flow-row grid-cols-6 gap-4' onSubmit={onSubmit}>
+      <div className='flex flex-col items-center h-screen '>
+        <div className='mt-10 h-96'>
+          {isLoading &&
+            <p>Loading...</p>
+          }
+          {!isLoading &&
+            <p>{answer?answer:""}</p>
+          } 
+        </div>
+        
+        <form className='grid grid-flow-row grid-cols-6 gap-4 bottom-0' onSubmit={onSubmit}>
           <textarea className='border-2 border-black rounded-md col-span-5' name="prompt" cols={50} />
           <button className='bg-sky-500 hover:bg-sky-700 text-white px-2 py-1 rounded-md col-span-1' type="submit">Ask IA</button>
         </form>
-        {isLoading &&
-          <p>Loading...</p>
-        }
-        {!isLoading &&
-          <p>{data?data.response:""}</p>
-        }
         
           
       </div>
